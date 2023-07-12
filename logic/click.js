028a6a0d874292b7452daa29163587f4f313d523
nabin2004 <olinabin2004@gmail.com>
2023-07-12 22:48:12 +0545

    Solved a bug for multiple selection option

--- logic/click.js
+++ logic/click.js
@@ -1,13 +1,29 @@
 // console.log("Checking")
 
+const clickedElements = [];
+
+
 const allsquare = document.getElementsByClassName("square")
 // console.log(allsquare)
 for (const x of allsquare){
     x.addEventListener("click",function() {
-        const idofElement = x.getAttribute("id")
+
+        
+        console.log(clickedElements)
+        // Problem is array is getting stored more and more clicked items but we will make it store only one element
+        const idofElement = x.getAttribute("id");
         const innerHTMLofElement = document.getElementById(idofElement).innerHTML;
-        if (innerHTMLofElement.includes('black') || innerHTMLofElement.includes('white')){
-            document.getElementById(idofElement).style.backgroundColor = "#f78f3f"
+        if(clickedElements.length>0){
+            clickedElements[0].removeAttribute("style");
+            clickedElements.length = 0;
+        }
+       
+
+        if (innerHTMLofElement.includes('black') || 
+        innerHTMLofElement.includes('white'))
+        {
+            document.getElementById(idofElement).style.backgroundColor = "#f78f3f";
+            clickedElements.push(x);
         }
         });
     }
